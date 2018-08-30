a51ed1443312f6c373709daac1852321b4a275ff
Corentin Crébier <crebier.corentin@gmail.com>
2018-08-30 08:10:50 +0200

    DB prod

---
+++ ormconfig.js
@@ -0,0 +1,79 @@
+let config;
+
+if (process.env.NODE_ENV === "production") {
+    config = {
+        "type": process.env.DB_TYPE,
+        "database": process.env.DB,
+        "host": process.env.DB_HOST,
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "charset": "utf8mb4",
+        "synchronize": false,
+        "logging": false,
+        "entities": [
+            "database/entity/**/*.ts"
+        ],
+        "migrations": [
+            "database/migration/**/*.ts"
+        ],
+        "subscribers": [
+            "database/subscriber/**/*.ts"
+        ],
+        "cli": {
+            "entitiesDir": "database/entity",
+            "migrationsDir": "database/migration",
+            "subscribersDir": "database/subscriber"
+        }
+    };
+}
+else {
+    config = [
+        {
+            "name": "default",
+            "type": "sqlite",
+            "database": "database.sqlite",
+            "synchronize": true,
+            "logging": false,
+            "entities": [
+                "database/entity/**/*.ts"
+            ],
+            "migrations": [
+                "database/migration/**/*.ts"
+            ],
+            "subscribers": [
+                "database/subscriber/**/*.ts"
+            ],
+            "cli": {
+                "entitiesDir": "database/entity",
+                "migrationsDir": "database/migration",
+                "subscribersDir": "database/subscriber"
+            }
+        },
+        {
+            "name": "test",
+            "type": "sqlite",
+            "database": ":memory:",
+            "synchronize": false,
+            "dropSchema": true,
+            "migrationsRun": true,
+            "logging": false,
+            "entities": [
+                "database/entity/**/*.ts"
+            ],
+            "migrations": [
+                "database/migration/**/*.ts"
+            ],
+            "subscribers": [
+                "database/subscriber/**/*.ts"
+            ],
+            "cli": {
+                "entitiesDir": "database/entity",
+                "migrationsDir": "database/migration",
+                "subscribersDir": "database/subscriber"
+            }
+        }
+    ];
+}
+
+
+module.exports = config;

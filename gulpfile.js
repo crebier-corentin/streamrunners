66728736554ea5c0910282f5b14ccb5ae5cb684d
Corentin Crébier <crebier.corentin@gmail.com>
2018-09-03 09:46:56 +0200

    bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,14 @@ var cssnano = require('cssnano');
 var cssmqpacker = require('css-mqpacker');
 
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
     return gulp.src('./asset/sass/style.scss')
+       // .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([autoprefixer(), cssnano(), cssmqpacker()]))
+        //.pipe(sourcemaps.write())
         .pipe(gulp.dest('./public/css/'));
 });
 

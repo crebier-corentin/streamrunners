e42bc303357258c252bc1e586dbaf550119afc5f
Kévin Comte <chomawatube@gmail.com>
2019-09-01 23:27:27 +0200

    Correction

--- app.ts
+++ app.ts
@@ -60,7 +60,7 @@ createConnection().then(async () => {
     //Discord
     const client = new Discord.Client();
     await client.login(process.env.DISCORD_TOKEN);
-    client.user.setActivity("TV", {type: "WATCHING"})
+    client.user.setActivity({{ viewers }}"" membres connectés sur StreamRunners.fr", {type: "WATCHING"})
     app.use((req, res, next) => {
         req.discord = client;
         next();

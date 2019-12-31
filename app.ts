6eb7fcff86f3cf35ce09b3ab4db2b2e29eb2c81c
Øɱŋɘѕ - <chomawatube@gmail.com>
2019-12-31 09:56:51 +0100

    Bot Leveling Discord

--- app.ts
+++ app.ts
@@ -23,6 +23,9 @@ const twitchStrategy = require("passport-twitch-new").Strategy;
 const compression = require('compression');
 const helmet = require('helmet');
 const Discord = require("discord.js");
+const fs = require("fs");
+const config = require('./config.json');
+let db = JSON.parse(fs.readFileSync("./database.json", "utf8"));
 
 //.env
 require("dotenv").config();
@@ -102,6 +105,45 @@ createConnection().then(async () => {
 				});        
     }
 	});
+
+	//Niveau
+
+	client.on("message", message => {
+    if (message.author.bot) return; // ignore bots
+
+    // if the user is not on db add the user and change his values to 0
+    if (!db[message.author.id]) db[message.author.id] = {
+        xp: 0,
+        level: 0
+      };
+    db[message.author.id].xp++;
+    let userInfo = db[message.author.id];
+    if(userInfo.xp > 100) {
+        userInfo.level++
+        userInfo.xp = 0
+        message.reply("Bravo, tu montes de niveau !")
+    }
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if(cmd === "niveau") {
+        let userInfo = db[message.author.id];
+        let member = message.mentions.members.first();
+        let embed = new Discord.RichEmbed()
+        .setColor(0x4286f4)
+        .addField("Niveau", userInfo.level)
+        .addField("XP", userInfo.xp+"/100");
+        if(!member) return message.channel.sendEmbed(embed)
+        let memberInfo = db[member.id]
+        let embed2 = new Discord.RichEmbed()
+        .setColor(0x4286f4)
+        .addField("Niveau", memberInfo.level)
+        .addField("XP", memberInfo.xp+"/100")
+        message.channel.sendEmbed(embed2)
+    }
+    fs.writeFile("./database.json", JSON.stringify(db), (x) => {
+        if (x) console.error(x)
+      });
+})
 
     //Leaderboard
     client.on("message", async msg => {

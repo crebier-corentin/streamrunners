9e5c350d09ce79fd21c27eadb3db478097e3e40f
Corentin Crébier <crebier.corentin@gmail.com>
2020-01-25 18:32:27 +0100

    Raffle value migration

---
+++ database/migration/1579972020430-RaffleValue.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class RaffleValue1579972020430 implements MigrationInterface {
+    name = 'RaffleValue1579972020430'
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("ALTER TABLE `raffle` ADD `value` int NOT NULL DEFAULT 0", undefined);
+        await queryRunner.query("ALTER TABLE `stream_queue` CHANGE `start` `start` datetime NULL DEFAULT null", undefined);
+        await queryRunner.query("ALTER TABLE `user_power` CHANGE `expires` `expires` datetime NULL DEFAULT null", undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("ALTER TABLE `user_power` CHANGE `expires` `expires` datetime NULL", undefined);
+        await queryRunner.query("ALTER TABLE `stream_queue` CHANGE `start` `start` datetime NULL", undefined);
+        await queryRunner.query("ALTER TABLE `raffle` DROP COLUMN `value`", undefined);
+    }
+
+}

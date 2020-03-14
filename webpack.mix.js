2cf93934d548da49173e8ddfb3174cf0f117dec8
Corentin Crébier <crebier.corentin@gmail.com>
2020-03-14 21:40:13 +0100

    Whitelist slider classes in purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -47,4 +47,5 @@ mix.sass('asset/sass/style.scss', 'public/css/')
         enabled: process.env.NODE_ENV === 'production',
         folders: ['views', 'asset'],
         extensions: ['html', 'js', 'php', 'vue', 'nunj'],
+        whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /^left-/, /^center$/, /^right-/],
     });

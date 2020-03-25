54e766ebac41276f0e26331604e36602429a8083
Corentin Crébier <crebier.corentin@gmail.com>
2020-03-25 11:24:16 +0100

    Add cache on chat

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import CacheService from '../common/utils/cache-service';
 import { EntityService } from '../common/utils/entity-service';
 import { UserEntity } from '../user/user.entity';
 import { ChatMessageEntity } from './chat-message.entity';
 
 @Injectable()
 export class ChatService extends EntityService<ChatMessageEntity> {
+    private cache = new CacheService(120); //2 Minute cache
+
     public constructor(@InjectRepository(ChatMessageEntity) repo) {
         super(repo);
     }
 
     public addMessage(message: string, user: UserEntity): Promise<ChatMessageEntity> {
+        this.cache.del('messages');
+
         const chatMessage = new ChatMessageEntity();
         chatMessage.author = user;
         chatMessage.message = message;
@@ -18,18 +23,22 @@ export class ChatService extends EntityService<ChatMessageEntity> {
     }
 
     public async softDelete(messageId: number, deletedBy: UserEntity): Promise<ChatMessageEntity> {
+        this.cache.del('messages');
+
         const message = await this.byIdOrFail(messageId);
         message.deletedBy = deletedBy;
         return this.repo.save(message);
     }
 
     public getLastMessages(): Promise<ChatMessageEntity[]> {
-        return this.repo
-            .createQueryBuilder('chat')
-            .leftJoinAndSelect('chat.author', 'author')
-            .leftJoinAndSelect('chat.deletedBy', 'deletedBy')
-            .take(50)
-            .orderBy('chat.createdAt', 'DESC')
-            .getMany();
+        return this.cache.get('messages', () =>
+            this.repo
+                .createQueryBuilder('chat')
+                .leftJoinAndSelect('chat.author', 'author')
+                .leftJoinAndSelect('chat.deletedBy', 'deletedBy')
+                .take(50)
+                .orderBy('chat.createdAt', 'DESC')
+                .getMany()
+        );
     }
 }

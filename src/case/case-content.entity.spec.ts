2e2541c8fc167db4d4d03cd2b46dc191f12ee47d
Corentin Crébier <crebier.corentin@gmail.com>
2020-04-13 09:22:09 +0200

    Fix CaseContentEntity.color test

--- src/case/case-content.entity.spec.ts
+++ src/case/case-content.entity.spec.ts
@@ -6,8 +6,8 @@ describe('CaseContentEntity', () => {
             ['#ffd700', 1, 19],
             ['#cf0a1d', 20, 30],
             ['#2e006c', 31, 150],
-            ['#0f056b', 151, 250],
-            ['#808080', 251, 1000],
+            ['#33cc33', 151, 250],
+            ['#0066ff', 251, 1000],
         ])('should return %s for range %i - %i', (expected, min, max) => {
             for (let chance = min; chance <= max; chance++) {
                 const content = new CaseContentEntity();
